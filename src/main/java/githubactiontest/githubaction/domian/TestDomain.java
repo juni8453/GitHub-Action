bde295d8777b9fa2c248f36526556204ec3a5746
ByeoungJoon Jeon <juni8453@naver.com>
2022-06-16 23:18:53 +0900

    JPA 의존 설정

--- src/main/java/githubactiontest/githubaction/domian/TestDomain.java
+++ src/main/java/githubactiontest/githubaction/domian/TestDomain.java
@@ -1,4 +1,16 @@
 package githubactiontest.githubaction.domian;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class TestDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
